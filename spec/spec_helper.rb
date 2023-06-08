efdcf55eb9f3cd9a3f1731a1269cbdb2e12b330d
Vladimir <vova.com940@gmail.com>
2023-06-08 14:47:55 +0300

    3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require_relative '../lib/exchange_it'
+
+RSpec.configure do |config|
+  config.define_derived_metadata(file_path: %r{exchange_it/utils}) do |meta|
+    meta[:fast] = true
+  end
+end
