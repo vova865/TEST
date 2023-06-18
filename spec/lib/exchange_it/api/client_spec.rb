c07c79526d9f7988a3eb1e4c166d6d624a6cce75
Vladimir <vova.com940@gmail.com>
2023-06-18 13:09:39 +0300

    6

--- spec/lib/exchange_it/api/client_spec.rb
+++ spec/lib/exchange_it/api/client_spec.rb
@@ -4,30 +4,38 @@ RSpec.describe ExchangeIt::Api::Client do
   let(:test_client) { described_class.new(ENV.fetch('LOKALISE_API_TOKEN', 'fake')) }
   let(:project_id) { '123.abc' }
 
-  specify '#project' do
-    body = JSON.dump(
-      'project_id' => project_id,
-      'project_type' => 'localization_files',
-      'name' => 'My Web App',
-      'description' => '',
-      'created_at' => '2021-11-24 16:39:25 (Etc/UTC)'
-    )
+  # specify '#project' do
+  #   body = JSON.dump(
+  #     'project_id' => project_id,
+  #     'project_type' => 'localization_files',
+  #     'name' => 'My Web App',
+  #     'description' => '',
+  #     'created_at' => '2021-11-24 16:39:25 (Etc/UTC)'
+  #   )
+  #
+  #   stub_request(:get, 'https://api.lokalise.com/api2/projects/123.abc')
+  #     .with(headers: {
+  #             'Accept' => 'application/json',
+  #             'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+  #             'User-Agent' => 'ruby-client',
+  #             'X-Api-Token' => '123abc'
+  #           })
+  #     .to_return(status: 200, body: body, headers: {})
+  #
+  #   project = test_client.project(project_id)
+  #   expect(project['project_id']).to eq(project_id)
+  #   expect(WebMock).to have_requested(
+  #     :get, 'https://api.lokalise.com/api2/projects/123.abc'
+  #   ).once
+  # end
 
-    stub_request(:get, 'https://api.lokalise.com/api2/projects/123.abc')
-      .with(headers: {
-              'Accept' => 'application/json',
-              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'User-Agent' => 'ruby-client',
-              'X-Api-Token' => '123abc'
-            })
-      .to_return(status: 200, body: body, headers: {})
-
-    project = test_client.project(project_id)
-    expect(project['project_id']).to eq(project_id)
-    expect(WebMock).to have_requested(
-      :get, 'https://api.lokalise.com/api2/projects/123.abc'
-    ).once
+  specify "project" do
+    project = VCR.use_cassette('projects/project') do
+      test_client.project('1234515414514514523145')
+    end
+    puts project.inspect
   end
+
 
   specify 'projects' do
     body = JSON.dump([
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,24 @@ require 'dotenv/load'
 require 'webmock/rspec'
 require_relative '../lib/exchange_it'
 
+Dir["#{File.dirname(__FILE__ )}/support/**/*.rb"].sort.each { |f| require f }
+
 RSpec.configure do |config|
   config.define_derived_metadata(file_path: %r{exchange_it/utils}) do |meta|
     meta[:fast] = true
   end
+
+  WebMock.allow_net_connect!
+
+  WebMock::API.prepend(Module.new do
+    extend self
+
+    def stub_request(*args)
+      VCR.turn_off!
+      super
+    end
+  end)
+
+  config.before{ VCR.turn_on! }
 end
+
